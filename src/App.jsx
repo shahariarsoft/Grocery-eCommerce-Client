9a5a278de46d344ecc6b3b5c4da2184bd58b1c12
Shahariar <mononshahariar@gmail.com>
2026-01-28 11:22:57 +0600

    seller path

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,11 @@ import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
 import SingleProduct from './pages/SingleProduct';
 import Address from './pages/Address';
-
-
+import SellerLogin from "./components/seller/SellerLogin";
+import SellerLayout from './pages/seller/SellerLayout';
+import ProductList from './pages/seller/ProductList';
+import AddProduct from './pages/seller/AddProduct';
+import Orders from './pages/seller/Orders';
 
 
 
@@ -35,6 +38,24 @@ const App = () => {
                 <Route path="/cart" element={<Cart />} />
                 <Route path="/my-orders" element={<MyOrders />} />
                 <Route path="/add-address" element={<Address />} />
+
+
+                <Route
+                path="/seller"
+                element={isSeller ? <SellerLayout /> : <SellerLogin/>}
+                >
+                  <Route
+                  index
+                  element={isSeller ? <AddProduct /> : null}
+                  />
+                  <Route
+                  path="product-list"
+                  element={isSeller ? <ProductList /> : null}
+                  />
+                  <Route
+                  path="orders" element={isSeller ? <Orders /> : null }
+                  />
+                </Route>
             </Routes>
         </div>
         {isSellerPath?null : <Footer />}
