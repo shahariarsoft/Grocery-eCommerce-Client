dd082f51f4eb555dd5bc5795f7c724a44c25493b
Shahariar <mononshahariar@gmail.com>
2026-01-29 13:42:10 +0600

    my orders page complete

--- src/pages/seller/SellerLayout.jsx
+++ src/pages/seller/SellerLayout.jsx
@@ -6,7 +6,7 @@ import { NavLink, Outlet } from "react-router-dom";
 
 
 const SellerLayout = () => {
-  const {isSeller,setIsSeller, navigate}=useContext(AppContext)
+    const {isSeller,setIsSeller, navigate}=useContext(AppContext)
 
     const sidebarLinks = [
         { name: "Add Product", path: "/seller", icon: assets.add_icon },
@@ -22,15 +22,15 @@ const SellerLayout = () => {
                     <p>Hi! Admin</p>
                     <button 
                     onClick={() => {
-                      setIsSeller(false);
-                      navigate("/");
+                        setIsSeller(false);
+                        navigate("/");
                     }}
-                    className='border rounded-full text-sm px-4 py-1'>Logout</button>
+                    className='border rounded-full text-sm px-4 py-1 cursor-pointer'>Logout</button>
                 </div>
             </div>
 
             <div className="flex">
-              <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
+                <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
                 {sidebarLinks.map((item) => (
                     <NavLink
                         to={item.path} 
@@ -38,9 +38,9 @@ const SellerLayout = () => {
                         end={item.path == "/seller"}
                         className={({ isActive }) => `flex items-center py-3 px-4 gap-3
                         ${
-                          isActive
-                          ?"border-r-4 md:border-r-[6px] bg-indigo-50 border-indigo-500 text-indigo-500"
-                          : "hover:bg-gray-100/90 border-white"
+                            isActive
+                            ?"border-r-4 md:border-r-[6px] bg-indigo-50 border-indigo-500 text-indigo-500"
+                            : "hover:bg-gray-100/90 border-white"
                         }`}
                     >
                         <img src={item.icon} alt="" className="w-7 h-7" />
