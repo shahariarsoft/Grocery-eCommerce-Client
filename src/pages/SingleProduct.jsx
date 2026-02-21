d034017971ecf3574f593f7731ff4bf25562e782
Monon Shahariar <mononshahariar@gmail.com>
2026-02-21 09:24:56 +0600

    Update SingleProduct.jsx

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -45,7 +45,7 @@ const SingleProduct = () => {
                   className="border max-w-24 border-gray-500/30 rounded overflow-hidden cursor-pointer"
                 >
                   <img
-                    src={`http://localhost:5000/images/${image}`}
+                    src={`https://grocery-ecommerce-server.onrender.com/images/${image}`}
                     alt={`Thumbnail ${index + 1}`}
                   />
                 </div>
@@ -54,7 +54,7 @@ const SingleProduct = () => {
 
             <div className="border border-gray-500/30 max-w-100 rounded overflow-hidden">
               <img
-                src={`http://localhost:5000/images/${thumbnail}`}
+                src={`https://grocery-ecommerce-server.onrender.com/images/${thumbnail}`}
                 alt="Selected product"
               />
             </div>
